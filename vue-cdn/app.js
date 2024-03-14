c6daafdb08fa9108e044bc25f6d7c8ab6259bb35
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-03-14 06:35:54 -0300

    3: v-for, attribute binding, dynamic classes, computed

--- vue-cdn/app.js
+++ vue-cdn/app.js
@@ -2,17 +2,54 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      title: 'Some title',
-      author: 'Some author',
-      age: 100,
+      books: [
+        {
+          id: 1,
+          title: 'Book 1',
+          author: 'Author 1',
+          cover: 'assets/1.jpg',
+          isFav: true,
+        },
+        {
+          id: 2,
+          title: 'Book 2',
+          author: 'Author 2',
+          cover: 'assets/2.jpg',
+          isFav: false,
+        },
+        {
+          id: 3,
+          title: 'Book 3',
+          author: 'Author 3',
+          cover: 'assets/3.jpg',
+          isFav: true,
+        },
+      ],
+      x: 0,
+      y: 0,
     }
   },
   methods: {
-    changeTitle(newTitle) {
-      this.title = newTitle
-    },
     toggleShowBooks() {
       this.showBooks = !this.showBooks
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type)
+      if (data) {
+        console.log(data)
+      }
+      if (e.type === 'mousemove') {
+        this.x = e.offsetX
+        this.y = e.offsetY
+      }
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav
+    },
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav)
     },
   },
 })
